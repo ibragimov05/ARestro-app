9ac35293386fd8d6f0975ce46685f32c92bdb89d
ibragimov05 <ibragimov.asus15@gmail.com>
2024-05-17 20:15:51 +0500

    first commit

---
+++ lib/generated/assets.dart
@@ -0,0 +1,56 @@
+class Assets {
+  Assets._();
+
+  //! icons
+  /// food_order_icons
+  static const String iconStarYellow =
+      'assets/icons/food_order_icons/star/star_yellow.svg';
+  static const String iconStarSelected =
+      'assets/icons/food_order_icons/star/selected_star.svg';
+  static const String iconStarUnSelected =
+      'assets/icons/food_order_icons/star/unselected_star.svg';
+  static const String iconCart = 'assets/icons/food_order_icons/cart.svg';
+
+  /// main_icons
+  static const String iconFire = 'assets/icons/main_icons/fire.svg';
+  static const String iconStar = 'assets/icons/main_icons/star.svg';
+
+  /// onboarding_icons
+  static const String iconGoogle = 'assets/icons/onboarding_icons/google.png';
+
+  /// search_bar_icons
+  static const String iconKeyboardVoice =
+      'assets/icons/search_bar_icons/keyboard_voice';
+  static const String iconSearch = 'assets/icons/search_bar_icons/search.svg';
+
+  /// tab_box_icons
+  static const String iconTabBoxHome = 'assets/icons/tab_box_icons/home.svg';
+  static const String iconTabBoxSearch =
+      'assets/icons/tab_box_icons/search.svg';
+  static const String iconTabBoxCart = 'assets/icons/tab_box_icons/cart.svg';
+
+  //! images
+  /// dashboard_images
+  static const String imageThreeBurger =
+      'assets/images/dashboard_images/three_burger.png';
+  static const String imageFood1 = 'assets/images/dashboard_images/food1.png';
+  static const String imageFood2 = 'assets/images/dashboard_images/food2.png';
+  static const String imageFood3 = 'assets/images/dashboard_images/food3.png';
+
+  /// food_order_images
+  static const String imageLittleBackground =
+      'assets/images/food_order_images/little_background.png';
+  static const String imageMainBurger =
+      'assets/images/food_order_images/main_burger.png';
+  static const String imageBackground =
+      'assets/images/food_order_images/background.png';
+
+  /// main_images
+  static const String imageMainLogo = 'assets/images/main_photos/main_logo.svg';
+
+  /// viewall_images
+  static const String imageMomo = 'assets/images/viewall_images/momo.png';
+  static const String imagePizza = 'assets/images/viewall_images/pizza.png';
+  static const String imageRestaurant =
+      'assets/images/viewall_images/restaurant.png';
+}
---
+++ lib/utils/colors/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color appBackgroundColor = Color(0xFFFFFFFF);
+  static const Color mainGreenColor = Color(0xFF009944);
+  static const Color textColorBlack = Color(0xFF000000);
+  static const Color forgotPassword = Color(0xFFE86969);
+  static const Color onboardingBlueColor = Color(0xFF2F89FC);
+  static Color containerBackgroundColor =
+      const Color(0xFF004422).withOpacity(10);
+  static Color mainTextGreyColor = const Color(0xFF000000).withOpacity(0.5);
+  static const Color greenColorBestOffers = Color(0xFF009944);
+}
